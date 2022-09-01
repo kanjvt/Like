9ea6de5e2e7807d328b1510c134e05cba3eac01c
kanjvt <90333832+kanjvt@users.noreply.github.com>
2022-09-01 19:05:59 +0700

    updt

--- vps.py
+++ vps.py
@@ -1,7 +1,7 @@
 import os, sys
 
 #key = "IG_USERNAME"
-link = "https://www.instagram.com/p/Cg6X457P7FV/"
+link = "https://www.instagram.com/p/ChKSWxNr2En/"
 #value = os.getenv(key)
 while True:
     os.system(f"python3 like.py {link}")
